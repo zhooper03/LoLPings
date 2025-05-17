bedd500e9b39a65337c9cc45966b7cdba01fd5e5
Zachary Hooper <the1errandboy@gmail.com>
2025-05-17 15:04:52 -0400

    Initial commit

---
+++ ExamplePlayerControllerPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace LoLPings
+{
+    [HarmonyPatch(typeof(PlayerController))]
+    internal static class ExamplePlayerControllerPatch
+    {
+        [HarmonyPrefix, HarmonyPatch(nameof(PlayerController.Start))]
+        private static void Start_Prefix(PlayerController __instance)
+        {
+            // Code to execute for each PlayerController *before* Start() is called.
+            LoLPings.Logger.LogDebug($"{__instance} Start Prefix");
+        }
+
+        [HarmonyPostfix, HarmonyPatch(nameof(PlayerController.Start))]
+        private static void Start_Postfix(PlayerController __instance)
+        {
+            // Code to execute for each PlayerController *after* Start() is called.
+            LoLPings.Logger.LogDebug($"{__instance} Start Postfix");
+        }
+    }
+}
---
+++ LoLPings.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using REPOLib;
+
+namespace LoLPings
+{
+    [BepInPlugin("VanillaBlanket.LoLPings", "LoLPings", "1.0")]
+    [BepInDependency(REPOLib.MyPluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
+    public class LoLPings : BaseUnityPlugin
+    {
+        internal static LoLPings Instance { get; private set; } = null!;
+        internal new static ManualLogSource Logger => Instance._logger;
+        private ManualLogSource _logger => base.Logger;
+        internal Harmony? Harmony { get; set; }
+
+        private void Awake()
+        {
+            Instance = this;
+
+            // Prevent the plugin from being deleted
+            this.gameObject.transform.parent = null;
+            this.gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+            Patch();
+            
+            Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
+        }
+
+        internal void Patch()
+        {
+            Harmony ??= new Harmony(Info.Metadata.GUID);
+            Harmony.PatchAll();
+        }
+
+        internal void Unpatch()
+        {
+            Harmony?.UnpatchSelf();
+        }
+
+        private void Update()
+        {
+            // Code that runs every frame goes here
+        }
+    }
+}
